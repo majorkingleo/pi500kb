1576568b2ae56a5724905b7843ec232b85f8103c
Martin Oberzalek <kingleo@gmx.at>
2026-07-20 22:51:34 +0200

    added build and install scripts

--- configure-pi500.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! test -d build ; then
-	mkdir build
-fi
-
-cd build && cmake .. -DKEYBOARD_VID=0x2e8a -DKEYBOARD_PID=0x0010 -DKEYBOARD_DEV=/dev/input/by-id/usb-Raspberry_Pi_Ltd_Pi_500_Keyboard-event-kbd
